58fac11931dfff874c841171f19c093064a26033
beyzakoc2 <beyzab2@icloud.com>
2026-02-11 16:50:53 +0300

    interface and abstract classes

---
+++ abstractClasses.ts
@@ -0,0 +1,39 @@
+abstract class KrediBase {
+  constructor() {}
+  kaydet(): void {
+    console.log("kaydedildi");
+  }
+
+  abstract hesapla(): void;
+}
+class TuketiciKredi extends KrediBase {
+  constructor() {
+    super();
+  }
+  hesapla(): void {
+    console.log("Tüketici Kredisine göre hesap yapıldı");
+  }
+}
+
+class MortgageKredi extends KrediBase {
+  constructor() {
+    super();
+  }
+  hesapla(): void {
+    console.log("Konut Kredisine göre hesap yapıldı");
+  }
+  baskaBirOperasyon() {}
+}
+
+let tuketiciKredisi = new TuketiciKredi();
+tuketiciKredisi.hesapla();
+tuketiciKredisi.kaydet();
+
+let mortgageKredisi = new MortgageKredi();
+mortgageKredisi.hesapla();
+mortgageKredisi.kaydet();
+
+let kredi: KrediBase;
+kredi = new TuketiciKredi();
+
+kredi = new MortgageKredi();
